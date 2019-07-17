d947e25f14d96c719ce6699bc5a265469b3a8e49
SZ76 <52050047+SZ76@users.noreply.github.com>
2019-07-17 12:50:34 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,49 @@ import 'package:sleep/pages/startPage.dart';
 import 'package:sleep/pages/statisticsPage.dart';
 import "pages/homePage.dart";
 import 'pages/aboutPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
+
+int milleseconds = 0;
+int addmilli = 0;
+var startup = false;
+String name = "name";
+bool nameSave = true;
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
+  
+  static saveload() {
+    void Save() async{
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setInt('milli', milleseconds);
+      prefs.setString("name", name) ?? '';
+    }
+    void Load() async{
+      final prefs = await SharedPreferences.getInstance();
+      addmilli = prefs.getInt('milli');
+      name = prefs.getString('name');
+      print("oof" + name);
+    }
+
+    void awaitsave() async{
+      await Save();
+    }
+    awaitsave();
+    
+    if (!startup){
+      Load();
+      startup = !startup;
+    }else {
+      milleseconds = addmilli + stop.elapsedMilliseconds;
+    }
+  }
+
   Widget build(BuildContext context) {
+    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: PageView(
@@ -25,6 +61,6 @@ class MyApp extends StatelessWidget {
               ],)
         ],
       ),
-      );
+    );
   }
 }
